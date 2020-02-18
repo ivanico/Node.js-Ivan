ebc0e383918190f95ea3e1a0127eddbb28e35b1a
ivanico <trudno.pile3@gmail.com>
2020-02-18 21:53:50 +0100

    c6

--- Node-c/index.js
+++ Node-c/index.js
@@ -8,11 +8,18 @@ const bodyParser = require('body-parser') //npm install body-parser
 
 const server = express();
 
+
+const mongoose = require ('mongoose') //npm install mongoose
+
+const models = require('./src/Models');
+
 server.use(cors());
 
 const port = 3001;
 
-const localStorage = [];
+
+
+// const localStorage = [];
 
 // localStorage.push({isbn:`1`, name:`Harry Potter`})
 // localStorage.push({isbn:`2`, name:`World of warcraft`})
@@ -28,12 +35,35 @@ server.use(bodyParser.json())
 // localStorage['2'] = 'Mirko';
 // localStorage['3'] = 'Stanko';
 
-routes.Routes(server, localStorage);                       //vaka se povikuva
+routes.Routes(server);                       //vaka se povikuva
+
+mongoose.connect(`mongodb+srv://ico:ico@cluster0-j520k.mongodb.net/test?retryWrites=true&w=majority`,{useNewUrlParser: true , useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on("error", () => {console.log('error connecting'+ error)});
+db.once('open', () => {
+    console.log('Successfully connected to MDB')
+    server.listen(port, () => {
+        console.log(`Server started on port ${port}, hello world!`);
+        
+        const firstBook = new models.Book({
+            isbn:1,
+            title:'Chernobyl',
+            author:'Pero',
+            year:'1976'
+        })             //bez const firstBook moze da se sejvne posle objektot.save
+        
+        firstBook.save(( err , book ) => {
+            if (err) {
+                console.log('Data was not saved :' + err)
+            }
+            else{
+                console.log(book)
+            }
+        });
+    });
+})
 
 
-server.listen(port, () => {
-    console.log(`Server started on port ${port}, hello world!`);
-    
-});
 
 
